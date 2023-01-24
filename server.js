6f79e8f8eefc1753a9ed49ced33117992a3a24fd
lonelydomino <lonelydomino@icloud.com>
2023-01-23 22:17:15 -0800

    stuff

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ if(process.env.NODE_ENV === "production"){
     ))
 }
 
-const port = process.env.PORT || 5000
-app.listen(port, console.log(`server listening to port 5000 only`))
+const port = process.env.PORT || 5001
+app.listen(port, console.log(`server listening to port 5001 only`))
